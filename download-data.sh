9cd0644e8c2a1efa6c177604d4d41c580d6a6fbd
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2019-01-16 12:11:20 -0800

    Script to download data

---
+++ download-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names
+
+
